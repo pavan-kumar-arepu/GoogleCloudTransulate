b638da15c98a1f3bbb3ed494e252d6522d89df2b
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2023-08-31 19:31:23 +0200

    Update the UI

--- GoogleCloudTransulate/GoogleCloudTransulate/Transulate.swift
+++ GoogleCloudTransulate/GoogleCloudTransulate/Transulate.swift
@@ -9,15 +9,14 @@ import Foundation
 
 import SwiftUI
 
-
 struct TransulateView: View {
-
+    
     
     @State private var inputText = "How are you"
     @State private var selectedLanguage = "Hindi" // Default target language
-
+    
     @State private var translatedText: String?
-    let apiKey = "************************"
+    let apiKey = "AIzaSyDtYovVn0-mrX9BwSu1_2b4-72tqlQLp0I"
     
     let indianLanguages: [String: String] = [
         "Assamese": "as",
@@ -43,57 +42,65 @@ struct TransulateView: View {
         "Telugu": "te",
         "Urdu": "ur"
     ]
-
+    
     var body: some View {
+        
         VStack {
+            
             Text("Transulation App").font(.title)
             Spacer()
             
             Text("India is an incredibly linguistically diverse country, and the number of languages spoken can vary depending on how languages and dialects are classified.")
+                .foregroundColor(.blue)
+            // Set text color to white
             
             let viewHeight = UIScreen.main.bounds.height * 0.2 /// Calculate 30% of the screen height
             Spacer()
-
+            
             TextField("Enter text to translate", text: $inputText)
                 .multilineTextAlignment(.center)
                 .frame(height: viewHeight)
-                .padding()
+                .padding(7)
                 .background(Color.gray.opacity(0.1))
                 .cornerRadius(10)
+                .shadow(radius: 4.0)
+                .lineLimit(5)
             
-                Text("Translated Text:")
-            Text(translatedText ?? "No translation available")
+            Text(translatedText ?? "Didn't try below languages yet?")
                 .frame(height: viewHeight)
-                .background(Color.gray.opacity(0.1))
                 .padding(.horizontal, 10)
                 .frame(maxWidth: .infinity) // Expand to full width
                 .cornerRadius(10)
-                .padding(.horizontal, 10) // Spacing on the left and right
+                .padding(.horizontal, 10)
+                .foregroundColor(.gray)// Spacing on the left and right
             
-            Spacer()
-
-            HStack {
-                Picker("Select a language", selection: $selectedLanguage) {
+            // Replace the Picker with a Grid of buttons
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: [GridItem(.adaptive(minimum: 80))]) {
                     ForEach(indianLanguages.keys.sorted(), id: \.self) { language in
-                        Text(language).tag(language)
+                        Button(action: {
+                            selectedLanguage = language
+                            translateText() // Automatically initiate translation
+                            
+                        }) {
+                            Text(language)
+                                .padding()
+                                .background(Color.gray.opacity(0.1))
+                                .cornerRadius(10)
+                        }
                     }
                 }
-                .pickerStyle(MenuPickerStyle())
-                .padding()
-                Button("Translate") {
-                    translateText()
-                }
+                .padding(.horizontal)
             }
         }
     }
-        
     
     func translateText() {
         guard let targetLanguageCode = TranslationUtility.languageNameToCode(selectedLanguage, indianLanguages: indianLanguages) else {
             print("Target language code not found.")
             return
         }
-
+        
         TranslationUtility.translateText(inputText: inputText, targetLanguage: targetLanguageCode, apiKey: apiKey) { translation in
             DispatchQueue.main.async {
                 self.translatedText = translation
@@ -102,6 +109,12 @@ struct TransulateView: View {
     }
 }
 
+
+struct TransulateView_Preview: PreviewProvider {
+    static var previews: some View {
+        TransulateView()
+    }
+}
 
 
 
@@ -140,18 +153,18 @@ class TranslationUtility {
             "q": inputText,
             "target": targetLanguage
         ]
-
+        
         guard let url = URL(string: baseURL)?.withQueries(params) else {
             completion(nil)
             return
         }
-
+        
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else {
                 completion(nil)
                 return
             }
-
+            
             if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
                let translations = json["data"] as? [String: Any],
                let translatedTexts = translations["translations"] as? [[String: Any]],
